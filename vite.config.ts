5c8d3123386a6e7fbfd9481eba23892220402939
Lucas127128 <kayauleung2024@gmail.com>
2026-07-28 08:40:14 +0000

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  css: {
+    transformer: 'lightningcss',
+  },
   plugins: [
     tailwindcss({ optimize: true }),
     sveltekit({
