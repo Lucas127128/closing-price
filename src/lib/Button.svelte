425848ebeb6b768c42508eeba1621d2d5d373a7f
Lucas Leung <kayauleung2024@gmail.com>
2026-08-05 19:23:07 +0800

    FIX: header shift after button click

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -7,6 +7,6 @@
 
 <button
   onclick={downloadOnClick}
-  class="text-base text-white bg-inherit cursor-pointer border-[3px] border-emerald-300 rounded-xl overflow-hidden p-3 transition-[transform_0.3s_ease,border_0.2s_ease] hover:border-4 hover:transform-[perspective(350px)_translateZ(15px)]"
+  class="cursor-pointer border-[3px] border-emerald-300 overflow-hidden rounded-xl p-3 transition-[transform_0.3s_ease,border_0.2s_ease] hover:border-4 hover:transform-[perspective(350px)_translateZ(15px)] transform-gpu"
   >{buttonText}</button
 >
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,7 +47,7 @@
   };
 </script>
 
-<h1 class="text-white font-extrabold text-3xl mt-6">
+<h1 class="font-extrabold place-self-center text-3xl mt-6">
   Closing Price Generator
 </h1>
 <main class="grid place-content-center grid-rows-[3em_1fr] p-4 gap-4">
